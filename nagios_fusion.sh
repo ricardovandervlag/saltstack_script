9e292405003d0dd09e3148c12b41278ae7c10513
Ricardo van der Vlag <ricadovandervlag@gmail.com>
2018-06-13 14:04:18 +0200

    Fixed extracting bug

--- nagios_fusion.sh
+++ nagios_fusion.sh
@@ -1,10 +1,10 @@
-sudo apt install build-essential libgd-dev openssl libssl-dev unzip apache2 -y
+sudo apt install build-essential libgd-dev openssl libssl-dev unzip apache2 -ycd
 sudo useradd nagios
 sudo groupadd nagcmd
 sudo usermod -a -G nagcmd nagios
 wget https://assets.nagios.com/downloads/nagiosfusion/4/fusion-4.1.3.tar.gz
-tar xpf fusion*.tar.gz
-mv fusion-4.1.3.tar.gz nagios
+tar tar xzvf fusion-4.1.3.tar.gz
+mvdir fusion-4.1.3 nagios
 cd nagios
 ./configure --with-nagios-group=nagios --with-command-group=nagcmd
 make -j4 all
