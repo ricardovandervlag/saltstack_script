18b04c2352e395bd1350b9d680e1bb81d510cc88
Ricardo van der Vlag <ricadovandervlag@gmail.com>
2018-06-20 10:30:31 +0200

    Added crendtial store

--- minion_scripts/setup_repo.sh
+++ minion_scripts/setup_repo.sh
@@ -1,2 +1,2 @@
-cd /home || mkdir /home
-salt "$1" cmd.run "git clone https://$2:$3@github.com/ricardovandervlag/saltsack_script.git"
+git config credential.helper store
+salt "$1" cmd.run "git clone https://$2:$3@github.com/ricardovandervlag/saltsack_script.git "
