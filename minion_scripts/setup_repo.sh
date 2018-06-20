d9ea2b2d649132fb86594519eb56792297336f23
Ricardo van der Vlag <ricadovandervlag@gmail.com>
2018-06-20 10:48:50 +0200

    changed dir test to saltsack_script

--- minion_scripts/setup_repo.sh
+++ minion_scripts/setup_repo.sh
@@ -1,2 +1,2 @@
 git config credential.helper store
-salt "$1" cmd.run "git clone https://$2:$3@github.com/ricardovandervlag/saltsack_script.git /home/test"
+salt "$1" cmd.run "git clone https://$2:$3@github.com/ricardovandervlag/saltsack_script.git /home/saltsack_script"
