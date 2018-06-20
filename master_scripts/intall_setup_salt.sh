7b2f9fe8069fb7a2e76d8dd6de23fd5bfa0c08ba
Ricardo van der Vlag <ricadovandervlag@gmail.com>
2018-06-20 11:54:31 +0200

    Added master salt script

---
+++ master_scripts/intall_setup_salt.sh
@@ -0,0 +1,10 @@
+# Add master IPv4 ad the end of the script
+
+# Install Salt 
+curl -L https://bootstrap.saltstack.com -o install_salt.sh
+sh install_salt.sh -M
+rm install_salt.sh
+
+# Setup master
+echo '#master: salt' >> /etc/salt/minion
+echo "master: $1" >> /etc/salt/minion
