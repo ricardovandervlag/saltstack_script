eae2483e60315c6836eabea003a34c99e6ebddfd
Ricardo van der Vlag <ricardovandervlag@gmail.com>
2018-06-27 14:28:53 +0200

    Bug fix

--- master_scripts/install_setup_salt.sh
+++ master_scripts/install_setup_salt.sh
@@ -4,7 +4,7 @@ sh install_salt.sh -M
 rm install_salt.sh
 
 # Setup master
-sh -c "echo '$1 salt' >> /etc/hosts"
+sh -c "echo '127.0.0.1 salt' >> /etc/hosts"
 
 # Enable services
 service salt-minion start
