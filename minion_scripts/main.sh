2646bb66323cf10974e5efe3f2642cabd6bc6d5f
Ricardo van der Vlag <ricardovandervlag@gmail.com>
2018-06-27 14:47:36 +0200

    Small bug fix

--- minion_scripts/main.sh
+++ minion_scripts/main.sh
@@ -1,4 +1,4 @@
 cd /home/saltstack_scripts
 echo 'Please enter the IPv4 addr for salt master'
 read maser_ip
-bash "minion_scripts/install_setup_salt.sh $maser_ip"
+bash minion_scripts/install_setup_salt.sh $maser_ip
