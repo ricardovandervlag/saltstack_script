5c8fdc67b079e62f51e2a9ef2d6ccb0f7fcd2e0f
Ricardo van der Vlag <ricadovandervlag@gmail.com>
2018-06-28 01:01:19 +0200

    Added update functions to main script

--- master_scripts/main.sh
+++ master_scripts/main.sh
@@ -3,3 +3,7 @@ cd /home/saltstack_scripts
 # Install and setup salt master
 bash master_scripts/install_setup_salt.sh
 bash accept_all_minions.sh
+# Update repo on all machines
+bash update_local_repo.sh
+# Update all machines
+bash update_upgrade_all.sh
