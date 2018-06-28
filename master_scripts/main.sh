a70148dad27d2cf3061e62fee7858c545cf795b6
Ricardo van der Vlag <ricardovandervlag@gmail.com>
2018-06-28 14:45:02 +0200

    Updated main.sh

--- master_scripts/main.sh
+++ master_scripts/main.sh
@@ -7,10 +7,10 @@ service salt-minion restart
 service salt-master restart
 # Print all keys
 salt-key --list-all
-echo 'If not all minions were listed/accepted, please press ^C and restart script'
-sleep 10
 # Accepting all minions
 salt-key --accept-all -y
+echo 'If not all minions were listed/accepted, please press ^C and restart script'
+sleep 10
 # Update all machines
 bash update_upgrade_all.sh
 # Install Nagios
