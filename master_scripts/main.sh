322f7a7ba549d230b84a401be4cddc921a9a76df
Ricardo van der Vlag <ricardovandervlag@gmail.com>
2018-06-28 11:45:31 +0200

    Fixed path bug

--- master_scripts/main.sh
+++ master_scripts/main.sh
@@ -1,7 +1,7 @@
 # Change directory to script repo
-cd /home/saltstack_scripts
+cd /home/saltstack_scriptsmaster_scripts
 # Install and setup salt master
-bash master_scripts/install_setup_salt.sh
+bash install_setup_salt.sh
 bash accept_all_minions.sh
 # Update repo on all machines
 bash update_local_repo.sh
